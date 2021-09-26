8d25931dd1ac390e66806a134c43a1a4795c9d34
Jun <junhyeok_1996@naver.com>
2021-09-27 01:00:48 +0900

    210927

--- string_10/11720_sumn_210927.py
+++ string_10/11720_sumn_210927.py
@@ -1,9 +1,11 @@
 import sys
 
 n=int(sys.stdin.readline())
-a=list(sys.stdin.readline().strip())
+#   1  # 
 # a=list(map(str,sys.stdin.readline().strip()))
 # b=0
 # for i in a:
 #     b+=int(i)
+# print(b)
+a=list(sys.stdin.readline().strip())
 print(sum(map(int,a)))
