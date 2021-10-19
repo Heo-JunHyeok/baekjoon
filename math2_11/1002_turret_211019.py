ccc72b404aac6d94fc87c96feba26505e5823a42
Jun <junhyeok_1996@naver.com>
2021-10-19 15:51:28 +0900

    두 점의 교점의 개수

--- math2_11/1002_turret_211019.py
+++ math2_11/1002_turret_211019.py
@@ -4,13 +4,13 @@ t=int(sys.stdin.readline())
 
 for i in range(t):
     x_1,y_1,r_1,x_2,y_2,r_2=map(int,sys.stdin.readline().split())
-
     d=(x_2-x_1)**2+(y_2-y_1)**2    
-    if x_1==x_2 and y_1==y_2 and r_1==r_2:
+
+    if x_1==x_2 and y_1==y_2 and r_1==r_2: #동심원
         print('-1')
-    elif (r_1+r_2)**2 == d or (r_2-r_1)**2==d:
+    elif (r_1+r_2)**2 == d or (r_2-r_1)**2==d: #외접, 내접
         print('1')
-    elif (r_2-r_1)**2<d<(r_1+r_2)**2:
+    elif (r_2-r_1)**2<d<(r_1+r_2)**2: #|r2-r1| < d < r2+r1
         print('2')
-    else:
+    else: #외부, 내부
         print('0')
