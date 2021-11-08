570d5a1b581d4dc8c8a62a65d84a893b32246bd2
Jun <junhyeok_1996@naver.com>
2021-11-08 22:55:44 +0900

    1순위 x, 2순위 y

--- sort_10/11650_coordinate_211106.py
+++ sort_10/11650_coordinate_211106.py
@@ -2,10 +2,11 @@ import sys
 
 n=int(sys.stdin.readline())
 a=[]
+
 for _ in range(n):
-    a.append(sys.stdin.readline().split())
+    a.append(list(map(int,sys.stdin.readline().split())))
 
-a.sort(reverse=0)
-
-for i in a:
-    print(' '.join(i))
+a.sort(key=lambda a : (a[0], a[1]))
+                
+for x,y in a:
+    print(x,y)
