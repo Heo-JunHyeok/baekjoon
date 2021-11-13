4bfea5ad48fe08044b68c0477ae26ddb88d49cf5
Jun <junhyeok_1996@naver.com>
2021-11-13 23:23:16 +0900

    백트래킹 재귀

---
+++ backtracking_8/15649_nm_211112.py
@@ -0,0 +1,18 @@
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+a = []
+
+def nm():
+    if len(a) == m:
+        print(' '.join(map(str, a)))
+        return
+    
+    for i in range(1, n+1):
+        if i in a:
+            continue
+        a.append(i)
+        nm()
+        a.pop()
+        
+nm()
