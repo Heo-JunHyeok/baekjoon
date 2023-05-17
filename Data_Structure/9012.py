e817ee9e0d6cd4ab9b088d41b876b6f8b0e4ccb9
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-05-17 12:08:56 +0900

    괄호

--- Data_Structure/9012.py
+++ Data_Structure/9012.py
@@ -1,16 +1,43 @@
+# 방법 1 : 구현
+# import sys
+
+# for _ in range(int(sys.stdin.readline())):
+#     vps = sys.stdin.readline().strip()
+
+#     if vps.count("(") != vps.count(")"):
+#         print("NO")
+#         continue
+
+#     while "()" in vps:
+#         vps = vps.replace("()", "")
+
+#     if len(vps) != 0:
+#         print("NO")
+#     else:
+#         print("YES")
+
+# 방법 2 : 스택
 import sys
+
+
+def stack(valid_ps):
+    result = []
+
+    if valid_ps.count("(") != valid_ps.count(")"):
+        return "NO"
+
+    for i in valid_ps:
+        if i == ")":
+            if len(result) == 0:
+                return "NO"
+            result.pop()
+        elif i == "(":
+            result.append(")")
+
+    return "YES"
+
 
 for _ in range(int(sys.stdin.readline())):
     vps = sys.stdin.readline().strip()
 
-    if vps.count("(") != vps.count(")"):
-        print("NO")
-        continue
-
-    while "()" in vps:
-        vps = vps.replace("()", "")
-
-    if len(vps) != 0:
-        print("NO")
-    else:
-        print("YES")
+    print(stack(vps))
