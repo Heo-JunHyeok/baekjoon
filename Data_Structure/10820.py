d44162779277ac26c57dad4adefe9c3ba1dad534
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-05-31 18:57:01 +0900

    문자열 분석

---
+++ Data_Structure/10820.py
@@ -0,0 +1,31 @@
+import sys
+
+result = [0] * 4
+total = []
+
+f = open("input.txt", "rt")
+
+while 1:
+    # s = f.readline().rstrip("\n")
+    s = sys.stdin.readline().rstrip("\n")
+
+    if s == "":
+        break
+
+    for i in s:
+        if i.islower():
+            result[0] += 1
+        elif i.isupper():
+            result[1] += 1
+        elif i == " ":
+            result[-1] += 1
+        else:
+            result[2] += 1
+
+    total.append(result)
+    result = [0] * 4
+
+f.close()
+
+for i in total:
+    print(*i)
