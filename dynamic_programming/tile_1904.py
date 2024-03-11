32eb5d2ada0a1b204659f85071beb085b1133a59
JunHyeok Heo <Juunsik29@gmail.com>
2024-03-11 18:01:28 +0900

    Delete dynamic_programming directory

--- dynamic_programming/tile_1904.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-arr = [format(bin(i)[2:]).zfill(n) for i in range(n * n)]
-
-for i in arr[:]:
-    if (i.count("0") % 2 == 1) or ("00" not in i):
-        arr.remove(i)
-
-print(len(arr) + 1, arr)
